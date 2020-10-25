b54463e33bc284e4b8cb3a3a1d55af55be2500ce
madv809 <62539108+madv809@users.noreply.github.com>
2020-10-25 22:07:49 +0700

    Add files via upload

--- ROADS.cpp
+++ ROADS.cpp
@@ -11,24 +11,27 @@
 
 using namespace std;
 const int INF = 1000000009;
-int dp[101][10001], n, k, r, t, cbest = INF;
-int best = INF;
-vector <int> a[101], l[101], c[101];
+int d1[101], d2[101], n, k, r, t, best;
+vector <int> a[101], b[101], l[101], c[101], lRe[101], cRe[101];
 bool vist[101];
 
-int go (int u, int cost)
+void dijsktra (int d[], vector <int> adj[], vector <int> c[])
 {
-    if (cost < 0) return INF;
-    if (u == 1)
-        return 0;
-    if (dp[u][cost] != -1) return dp[u][cost];
-    dp[u][cost] = INF;
-    for (int i = 0; i < a[u].size(); ++i)
+    memset(vist, 0, sizeof(vist));
+    FOR(i, 0, n) d[i] = INF;
+    d[n] = 0;
+    while (1)
     {
-        int v = a[u][i];
-        dp[u][cost] = min(dp[u][cost], l[u][i] + go(v, cost - c[u][i]));
+        int maxx = 0;
+        FOR(i, 1, n) if (!vist[i] && d[i] < d[maxx]) maxx = i;
+        if (maxx == 0) break;
+        vist[maxx] = 1;
+        for (int i = 0; i < adj[maxx].size(); ++i)
+        {
+            int v = adj[maxx][i];
+            if (!vist[v] && d[v] > d[maxx] + c[maxx][i]) d[v] = d[maxx] + c[maxx][i];
+        }
     }
-    return dp[u][cost];
 }
 
 void reset()
@@ -36,11 +39,52 @@ void reset()
     FOR(i, 1, n)
     {
         a[i].clear();
+        b[i].clear();
         l[i].clear();
         c[i].clear();
+        lRe[i].clear();
+        cRe[i].clear();
     }
-    memset(dp, -1, sizeof(dp));
     best = INF;
+}
+
+void go (int u, int lroad, int cw)
+{
+    if (lroad + d1[u] >= best || cw + d2[u] > k) return;
+    if (u == n)
+    {
+        best = min(best, lroad);
+        return;
+    }
+    vist[u] = 1;
+    for (int i = 0; i < a[u].size(); ++i)
+    {
+        int v = a[u][i];
+        if (!vist[v]) go(v, lroad + l[u][i], cw + c[u][i]);
+    }
+    vist[u] = 0;
+}
+
+void load()
+{
+        int u, v, lt, ct;
+        scanf("%d", &k);
+        scanf("%d", &n);
+        scanf("%d", &r);
+        reset();
+        FOR(i, 1, r)
+        {
+            scanf("%d%d%d%d", &u, &v, &lt, &ct);
+            lRe[v].push_back(lt);
+            cRe[v].push_back(ct);
+            l[u].push_back(lt);
+            c[u].push_back(ct);
+            a[u].push_back(v);
+            b[v].push_back(u);
+        }
+        dijsktra(d1, b, lRe);
+        dijsktra(d2, b, cRe);
+        memset(vist, 0, sizeof(vist));
 }
 
 int main()
@@ -48,23 +92,11 @@ int main()
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
     scanf("%d", &t);
-    int u, v, lt, ct;
     while(t--)
     {
-        scanf("%d", &k);
-        scanf("%d", &n);
-        scanf("%d", &r);
-        reset();
-        FOR(i, 1, r)
-        {
-            scanf("%d%d", &u, &v);
-            scanf("%d%d", &lt, &ct);
-            a[v].push_back(u);
-            l[v].push_back(lt);
-            c[v].push_back(ct);
-        }
-        best = go(n, k);
-        if (best < INF) printf("%d\n", best);
+        load();
+        go(1, 0, 0);
+        if (best != INF) printf("%d\n", best);
         else printf("-1\n");
     }
 }
