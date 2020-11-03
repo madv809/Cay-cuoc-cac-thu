fa25c067e4e6cee556176b6eb3de04150ebe807a
madv809 <62539108+madv809@users.noreply.github.com>
2020-11-04 00:32:32 +0700

    Add files via upload

--- CENTRE28.cpp
+++ CENTRE28.cpp
@@ -13,43 +13,80 @@
 #include <iomanip>
 
 using namespace std;
-int q[30001], s[30001], n, m;
+int q[30001], s[30001], pos[30001], heap[2*30005], nheap, n, m;
 vector <int> a[30001];
 vector <LL> c[30001];
 LL d1[30001], d2[30001], f1[30001], f2[30001];
-bool inq[30001];
+bool inq[30001], vist[30001];
+
+void up (LL d[], int u)
+{
+    int child = pos[u], par;
+    if (child == 0)
+        child = ++nheap;
+
+    par = child/2;
+
+    while (par)
+    {
+        if (d[heap[par]] <= d[u]) break;
+        heap[child] = heap[par];
+        pos[heap[child]] = child;
+        child = par;
+        par /= 2;
+    }
+    heap[child] = u;
+    pos[u] = child;
+}
+
+int pop (LL d[])
+{
+    int x = heap[1], u = heap[nheap--];
+    int par = 1, child = 2;
+
+    while (child <= nheap)
+    {
+        if (child + 1 <= nheap && d[heap[child + 1]] < d[heap[child]])
+            ++child;
+        if (d[heap[child]] >= d[u]) break;
+        heap[par] = heap[child];
+        pos[heap[par]] = par;
+        par = child;
+        child *= 2;
+    }
+
+    heap[par] = u;
+    pos[u] = par;
+
+    return x;
+
+}
 
 void BF (LL d[], LL f[], int s)
 {
     REP(i, 1, n) d[i] = 999999999;
-    memset(inq, 0, sizeof(inq));
+    //memset(inq, 0, sizeof(inq));
+    memset(pos, 0, sizeof(pos));
+    memset(vist, 0, sizeof(vist));
 
-    q[0] = s;
     d[s] = 0;
     f[s] = 1;
+    up(d, s);
 
-    int x = 0, y = 0;
-
-    while (x != (y + 1)%n)
+    while (nheap)
     {
-        int u = q[x];
-        x = (x + 1)%n;
-        inq[u] = 0;
+        int u = pop(d);
+        vist[u] = 1;
 
         for (int i = 0; i < a[u].size(); ++i)
         {
             int v = a[u][i];
-
+            if (vist[v]) continue;
             if (d[v] > d[u] + c[u][i])
             {
                 d[v] = d[u] + c[u][i];
                 f[v] = f[u];
-                if (!inq[v])
-                {
-                    y = (y + 1)%n;
-                    q[y] = v;
-                    inq[v] = 1;
-                }
+                up(d, v);
             }
             else if (d[v] == d[u] + c[u][i])
                 f[v] += f[u];
