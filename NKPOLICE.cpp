0e83d5fc3424838218c2c844f0f0f3d94b8eb06a
madv809 <62539108+madv809@users.noreply.github.com>
2021-01-06 20:35:20 +0700

    Update NKPOLICE.cpp

--- NKPOLICE.cpp
+++ NKPOLICE.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 #include <queue>
 #include <map>
-
+ 
 #define LL long long
 #define ULL unsigned long long
 #define FOR(i, a, b) for (int i = a; i < b; ++i)
@@ -23,16 +23,16 @@
 #define piL pair<int, LL>
 #define pLL pair<LL, LL>
 #define ar array
-
+ 
 using namespace std;
-
+ 
 const int mxn = 1e5 + 5;
 const int INF = 1e9;
 const LL BASE = 311;
 int low[mxn], child[mxn], num[mxn], cut_node[mxn], en[mxn], in[mxn], t, n, m;
 int sp[mxn][30], log2n;
 vector<int> a[mxn];
-
+ 
 void dfs (int p, int u)
 {
     sp[u][0] = p;
@@ -43,34 +43,33 @@ void dfs (int p, int u)
         ++child[u];
         dfs(u, v);
         low[u] = min(low[u], low[v]);
-
+ 
         if (u == 1 && child[u] >= 2) cut_node[u] = 1;
         else if (low[v] >= num[u]) cut_node[u] = 1;
     }
     en[u] = t;
 }
-
+ 
 bool is_par (int u, int v)  // return true nếu u là cha của v
 {
     return (in[u] <= in[v] && en[u] >= en[v]);
 }
-
+ 
 int find_par (int v, int c)
 {
     for (int i = log2n; i >= 0; --i)
             if (!is_par(sp[v][i], c) && is_par(c, sp[v][i])) v = sp[v][i];
     return v;
 }
-
+ 
 bool solve (int u, int v, int g1, int g2)
 {
     if (is_par(g2, g1)) swap(g1, g2);
     if (low[g2] < num[g2]) return 1;
-    if (u == g1 || u == g2 || v == g1 || v == g2) return 0;
-    if (is_par(g1, u) == is_par(g1, v)) return 1;
+    if (is_par(g2, u) == is_par(g2, v)) return 1;
     return 0;
 }
-
+ 
 bool solve2 (int u, int v, int c)
 {
     if (!cut_node[c]) return 1;
@@ -85,23 +84,23 @@ bool solve2 (int u, int v, int c)
     {
         if (is_par(c, u)) swap(u, v);
         v = find_par(v, c);
-
+ 
         if (low[v] < num[c]) return 1;
     }
     return 0;
 }
-
+ 
 int main()
 {
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
     cin >> n >> m;
     REP(i, 1, m){int u, v; scanf("%d%d", &u, &v); a[u].pb(v); a[v].pb(u);}
-
+ 
     dfs(0, 1); in[0] = -INF; en[0] = INF;
     log2n = (int)log2(n);
     REP(j, 1, log2n) REP(i, 1, n) sp[i][j] = sp[sp[i][j - 1]][j - 1];
-
+ 
     int q; cin >> q;
     REP(i, 1, q)
     {
@@ -120,6 +119,11 @@ int main()
             else cout << "no\n";
         }
     }
+    /*int r = 0;
+    int x[13] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+    FOR(i, 0, 13) FOR(j, 0, 13) if (i != j)
+    FOR(k, 0, 13) if (k != j && k != i) {++r; cout << "2 " << x[k] << ' ' << x[j] << ' ' << x[i] << endl;}
+    cout << r;*/
     /*cout << low[12] << ' ' << en[12] << endl;
     cout << low[8] << ' ' << en[8] << endl;
     cout << low[9] << ' ' << en[9] << endl;
